f96131084e1f77113ac097bde258ffa89a1c46d6
Moaz Khaled <moaz.khaled@hotmail.com>
2022-12-27 22:20:05 +0200

    add gialogs and intiate player model

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -6,11 +6,10 @@
 package tictactoe;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import tictactoe.screens.modes.ModeBase;
+import tictactoe.screens.dualmode.OnlineListBase;
 
 /**
  *
@@ -20,7 +19,7 @@ public class TicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = new ModeBase();
+        Parent root = new OnlineListBase();
         
         Scene scene = new Scene(root);
         
--- src/tictactoe/utils/Dialogs.java
+++ src/tictactoe/utils/Dialogs.java
@@ -1,8 +1,79 @@
 package tictactoe.utils;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 
-class Dialogs {
+
+public class Dialogs {
+    
+    public static JFXDialog createBlurSimpleDialog(String text,StackPane stackpane){
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        JFXDialog dialog = new JFXDialog(stackpane,dialogLayout,JFXDialog.DialogTransition.TOP);
+        
+        dialogLayout.setStyle("-fx-background-color: rgba(59,178,184,0.8 ); -fx-background-radius: 10 10 10 10 ;");
+        
+        Label dialogLabel = new Label(text);
+        dialogLabel.setFont(new Font("Comic Sans MS Bold", 20.0));
+        dialogLayout.setBody(dialogLabel);
+        
+        return dialog;
+    }
+    
+    public static JFXDialog createBlurRequestingDialog(String text,StackPane stackpane){
+        
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        JFXDialog dialog = new JFXDialog(stackpane,dialogLayout,JFXDialog.DialogTransition.TOP);
+        dialog.setOverlayClose(false);
+        
+        dialogLayout.setStyle("-fx-background-color: rgba(59,178,184,0.8 ); -fx-background-radius: 10;");
+        
+        dialogLayout.setPadding(new Insets(10));
+        
+        HBox actionParent = new HBox();
+ 
+        JFXButton acceptButton = new JFXButton("Accept");
+        JFXButton rejectButton = new JFXButton("Reject");
+        
+        
+        actionParent.setSpacing(20);
+        
+        acceptButton.setPadding(new Insets(10));
+        rejectButton.setPadding(new Insets(10));
+        
+        
+        
+        
+        acceptButton.setFont(new Font("Comic Sans MS Bold", 14.0));
+        acceptButton.setStyle("-fx-text-fill:WHITE; -fx-background-color: rgba(130,213,49,1.0); -fx-background-radius: 10;");
+        
+        rejectButton.setFont(new Font("Comic Sans MS Bold", 14.0));
+        rejectButton.setStyle("-fx-text-fill:WHITE;-fx-background-color: rgba(242,12,12,1.0); -fx-background-radius: 10;");
+        
+        acceptButton.setButtonType(JFXButton.ButtonType.RAISED);
+        rejectButton.setButtonType(JFXButton.ButtonType.RAISED);
+        
+        dialogLayout.getChildren().addAll(actionParent);
+        actionParent.getChildren().addAll(acceptButton,rejectButton);
+        actionParent.setAlignment(Pos.BOTTOM_CENTER);
+        
+        Label dialogLabel = new Label(text);
+        
+        
+        
+        dialogLabel.setFont(new Font("Comic Sans MS Bold", 20.0));
+        dialogLabel.setStyle("-fx-text-fill:WHITE;");
+        
+        dialogLayout.setBody(dialogLabel);
+        
+        return dialog;
+    }
     
     
-	
 }
