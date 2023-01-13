ce094da940c77a5e843bf74ab4e472f889195143
arwa <arwashams643@gmail.com>
2023-01-13 21:30:39 +0200

    modify Login

--- src/tictactoe/screens/authentication/LoginBase.java
+++ src/tictactoe/screens/authentication/LoginBase.java
@@ -23,9 +23,6 @@ import tictactoe.screens.dualmode.DualModeBase;
 import tictactoe.screens.dualmode.OnlineListBase;
 import tictactoe.utils.Dialogs;
 
-
-
-
 public class LoginBase extends BorderPane {
 
     protected final VBox vBox;
@@ -340,16 +337,19 @@ public class LoginBase extends BorderPane {
                     "Insert your Passowrd First");
 
         } else {
-            Player player = DualModeBase.network.login(userNameTextField.getText(), passwordField.getText());
-            if (player != null) {
-                System.out.println("loginBase done");
-                Parent pane = new OnlineListBase(stage, player);
-                stage.getScene().setRoot(pane);
-            } else {
-                userNameTextField.setText("");
-                passwordField.setText("");
+            navigateUser(userName, password, stage);
+        }
+    }
 
-            }
+    private void navigateUser(String userName, String password, Stage stage) {
+        Player player = DualModeBase.network.login(userNameTextField.getText(), passwordField.getText());
+        if (player != null) {
+            System.out.println("loginBase done");
+            Parent pane = new OnlineListBase(stage, player);
+            stage.getScene().setRoot(pane);
+        } else {
+            userNameTextField.setText("");
+            passwordField.setText("");
         }
     }
 
